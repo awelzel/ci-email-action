e369cbf5a012061ea937186f3f0f2c5c93729558
Jon Siwek <jsiwek@corelight.com>
2019-12-17 08:15:54 -0800

    Add email template

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ci-email-action
 GitHub action to send email via SMTP on CI check_suite failure
+
+Example workflow:
+
+```
+name: CI Email Notification
+on:
+  check_suite:
+    types: [completed]
+jobs:
+  notify:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Send CI Email Notification
+        uses: jsiwek/ci-email-action@master
+        env:
+          CI_APP_NAME: "Cirrus CI"
+          SMTP_HOST: ${{ secrets.MAIL_HOST }}
+          SMTP_PORT: ${{ secrets.MAIL_PORT }}
+          SMTP_USER: ${{ secrets.MAIL_USER }}
+          SMTP_PASS: ${{ secrets.MAIL_PASS }}
+          MAIL_FROM: ${{ secrets.MAIL_FROM }}
+          MAIL_TO: ${{ secrets.MAIL_TO }}
+```
+
+TODO: maintain this repo in zeek org?
+TODO: publish this action?
--- process.py
+++ process.py
@@ -1,3 +1,47 @@
 #! /usr/bin/env python3
 
-print('hello')
+import os
+import sys
+
+def error(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    sys.exit(1)
+
+def getenv(var):
+    rval = os.environ[var]
+
+    if not rval:
+        error(f'Error: environment variable not set: {var}')
+
+    return rval
+
+def send_mail(msg):
+    import smtplib
+    from email.mime.text import MIMEText
+
+    smtp_host = getenv('SMTP_HOST')
+    smtp_port = getenv('SMTP_PORT')
+    smtp_user = getenv('SMTP_USER')
+    smtp_pass = getenv('SMTP_PASS')
+    smtp_timeout = 30
+
+    mail_from = getenv('MAIL_FROM')
+    mail_to   = getenv('MAIL_TO')
+
+    ci_app_name = getenv('CI_APP_NAME')
+
+    msg  = MIMEText(msg)
+    msg['Subject'] = f'Unsuccessful check suite from {ci_app_name}'
+    msg['From'] = mail_from
+    msg['To'] = mail_to
+
+    s = smtplib.SMTP(host=smtp_host, port=smtp_port, timeout=smtp_timeout)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(smtp_user, smtp_pass)
+    s.sendmail(mail_from, [mail_to], msg.as_string())
+    s.quit()
+
+print("Sending email...")
+send_mail("Just testing email from the GH action works")
